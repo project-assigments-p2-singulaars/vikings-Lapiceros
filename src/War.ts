0131da3f1bf602a31b949ca52881a1b4d3ffd866
julio <jcsmerubia@gmail.com>
2024-04-16 14:17:52 +0200

    war is over

--- src/War.ts
+++ src/War.ts
@@ -1,1 +1,53 @@
-export class War {}
+import { Saxon } from "./Saxon";
+import { Viking } from "./Viking";
+
+export class War {
+    vikingArmy: Viking[] = [];
+    saxonArmy: Saxon[]=[];
+
+    addViking(viking: Viking){
+        this.vikingArmy.push(viking);
+    };
+    addSaxon(saxon: Saxon){
+        this.saxonArmy.push(saxon);
+    };
+
+    vikingAttack(){
+        let randomSaxon= Math.floor(Math.random()*this.saxonArmy.length);
+        let randomViking=Math.floor(Math.random()*this.vikingArmy.length);
+
+        this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
+
+        if (this.saxonArmy[randomSaxon].health <= 0) {
+            this.saxonArmy = this.saxonArmy.filter((saxon) =>{
+                return this.saxonArmy[randomSaxon] !== saxon;
+            })
+            return "A Saxon has died in combat"
+        }
+
+    };
+    saxonAttack(){
+        let randomSaxon= Math.floor(Math.random()*this.saxonArmy.length);
+        let randomViking=Math.floor(Math.random()*this.vikingArmy.length);
+
+        this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
+
+        if (this.vikingArmy[randomViking].health <= 0) {
+            this.vikingArmy = this.vikingArmy.filter((viking) =>{
+                return this.vikingArmy[randomViking] !== viking;
+            })
+        }else{
+            return `${this.vikingArmy[randomViking].name} has received ${this.saxonArmy[randomSaxon].strength} points of damage`
+
+        }
+
+    }
+    showStatus(){
+        if(this.saxonArmy.length === 0){
+           return 'Vikings have won the war of the century!'
+
+    }
+    ;
+    };
+
+}
